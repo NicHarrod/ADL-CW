497174d18762a38c073e0dfb2352135095cb2451
Valentina Velasco <xd22898@bristol.ac.uk>
2025-11-18 18:12:54 +0000

    FC layers and concatenation

--- siamese_CNN.py
+++ siamese_CNN.py
@@ -42,17 +42,29 @@ class SiameseCNN(nn.Module):
 
 
 
-
-
-
-
         # fully connected layers:
+              
+        # global avg pooling to reduce spatial dimension from 14 x14 to 1 x 1
+        self.gap = nn.AdaptiveAvgPool2d((1,1))
 
+        # first FC layer
+        self.fc1 = nn.Sequential (
+            nn.Linear (1024, 512),
+            nn.ReLU()
+        )
+
+        # second FC Layer: 512 -> 3 with dropout (p = 0.5)
+        self.fc2 = nn.Sequential (
+            nn.Dropout (p = 0.5),
+            nn.Linear (512,3)
+        )
 
 
 
     def convForward(self, x: torch.Tensor) -> torch.Tensor:
         
+  
+
 
 
         return x
@@ -67,8 +79,26 @@ class SiameseCNN(nn.Module):
         # 
         # 
         # 
-        # concatenate the outputs
-        # 
+
+        # concatenate the outputs to give 1x1x1024 - here x and y are results of convolution at layer 7 and 8 
+        feat_map_1 = self.gap (x)
+        feat_map_2 = self.gap (y)
+
+        # flatten the feature maps to turn (B,512,1,1) into (B,512)
+        flat_feat_map_1 = feat_map_1.flatten(start_dim=1)
+        flat_feat_map_2 = feat_map_2.flatten(start_dim=1)
+
+        concatenated_features = torch.cat ((flat_feat_map_1, flat_feat_map_2), dim=1)
+        print (f"concatenated shape: {concatenated_features.shape}")
+
+        # FC layer 1 
+        fc_output = self.fc1 (concatenated_features)
+
+        # FC layer 2
+        final_output = self.fc2 (fc_output)
+
+        
+
         # 
         # 
         # 
@@ -80,9 +110,11 @@ class SiameseCNN(nn.Module):
         return x
     
     
+    
     @staticmethod
     def initialise_layer(layer):
         if hasattr(layer, "bias"):
             nn.init.zeros_(layer.bias)
         if hasattr(layer, "weight"):
             nn.init.kaiming_normal_(layer.weight)
+
